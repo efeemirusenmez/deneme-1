c2b665b16046aed4cdb1a8cc1bda1c7f61a5ba60
efemirusenmez <102237319+efemirusenmez@users.noreply.github.com>
2022-07-26 10:05:07 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-<title >Deneme Emir</title>
+<title >Deneme-Emir</title>
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <link href="site.css" rel="stylesheet" >
