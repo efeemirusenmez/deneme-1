00dc2b3792fa9882b960a508d9f580e700729040
efemirusenmez <emir.usenmez@themax.media>
2022-08-10 14:20:19 +0300

    versiyon 14

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
       'driver' => 'pgsql',
         'host' => env('DB_HOST', 'mysql://b37f567b3f741c:f43585bf@eu-cdbr-west-03.cleardb.net/heroku_123bc0464991a9f?reconnect=true'),
         'port' => env('DB_PORT', '5432'),
-        'database' => env('DB_DATABASE', 'dbXXXXXXXXXXXX'),
+        'database' => env('DB_DATABASE', 'deneme'),
         'username' => env('DB_USERNAME', 'b37f567b3f741c'),
         'password' => env('DB_PASSWORD', 'f43585bf'),
         'charset' => 'utf8',
