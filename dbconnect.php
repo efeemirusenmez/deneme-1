2560436f8210fc084bf3b336951ba2d6e8ecac73
efemirusenmez <emir.usenmez@themax.media>
2022-08-10 14:59:11 +0300

    dbconncet database bilgilri girildi3

--- dbconnect.php
+++ dbconnect.php
@@ -4,7 +4,8 @@ $username = "b37f567b3f741c";
 $password = "f43585bf";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname="heroku_123bc0464991a9f", $username, $password);
+  //$conn = new PDO("mysql:host=$servername;dbname="heroku_123bc0464991a9f", $username, $password);
+  $conn = new PDO("mysql://b37f567b3f741c:f43585bf@eu-cdbr-west-03.cleardb.net/heroku_123bc0464991a9f?reconnect=true");
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  // echo "Connected successfully";
