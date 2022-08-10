552d6efe8e3578e81e0f2bc3601146cf92892d48
efemirusenmez <emir.usenmez@themax.media>
2022-08-10 14:29:42 +0300

    versiyon 16

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 'pgsql' => [
       'driver' => 'pgsql',
         'host' => env('DB_HOST', 'eu-cdbr-west-03.cleardb.net'),
-        'port' => env('DB_PORT', '5432'),
+        'port' => env('DB_PORT', '3306'),
         'database' => env('DB_DATABASE', 'deneme'),
         'username' => env('DB_USERNAME', 'b37f567b3f741c'),
         'password' => env('DB_PASSWORD', 'f43585bf'),
