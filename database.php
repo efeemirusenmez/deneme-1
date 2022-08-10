576ddb64f0c621032b56fa0b60ba0977cc0858d4
efemirusenmez <emir.usenmez@themax.media>
2022-08-10 15:05:04 +0300

    dbconncet database bilgilri girildi4

--- database.php
+++ database.php
@@ -1,14 +1,13 @@
 <?php
-'pgsql' => [
-      'driver' => 'pgsql',
-        'host' => env('DB_HOST', 'eu-cdbr-west-03.cleardb.net'),
-        'port' => env('DB_PORT', '3306'),
-        'database' => env('DB_DATABASE', 'heroku_123bc0464991a9f'),
-        'username' => env('DB_USERNAME', 'b37f567b3f741c'),
-        'password' => env('DB_PASSWORD', 'f43585bf'),
-        'charset' => 'utf8',
-        'prefix' => '',
-        'schema' => 'public',
-        'sslmode' => 'prefer',
-    ]
+'mysql' => array(
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'SchoolBoard',
+        'username'  => 'root',
+        'password'  => 'venturaa',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+        'unix_socket' => '/tmp/mysql.sock',
+    ),
     ?>
