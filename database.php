47e0863a4c11ef9e72fbcbfbb4a8f3c74c5e6ba3
efemirusenmez <emir.usenmez@themax.media>
2022-08-10 14:27:50 +0300

    versiyon 15

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 'pgsql' => [
       'driver' => 'pgsql',
-        'host' => env('DB_HOST', 'mysql://b37f567b3f741c:f43585bf@eu-cdbr-west-03.cleardb.net/heroku_123bc0464991a9f?reconnect=true'),
+        'host' => env('DB_HOST', 'eu-cdbr-west-03.cleardb.net'),
         'port' => env('DB_PORT', '5432'),
         'database' => env('DB_DATABASE', 'deneme'),
         'username' => env('DB_USERNAME', 'b37f567b3f741c'),
