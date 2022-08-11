39d72bc8fe17260c26776053da57444d67f62d99
efemirusenmez <emir.usenmez@themax.media>
2022-08-11 12:37:38 +0300

    db connect hata deneme 2

--- dbconnect.php
+++ dbconnect.php
@@ -11,10 +11,10 @@
 $servername = "eu-cdbr-west-03.cleardb.net";
 $username = "b37f567b3f741c";
 $password = "f43585bf";
-$dbname = "CLEARDB_DATABASE_URL";
+$dbname = "heroku_123bc0464991a9f";
 
 try {
-  $conn = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=CLEARDB_DATABASE_URL', $username, $password);
+  $conn = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_123bc0464991a9f', $username, $password);
   //$conn = new PDO("mysql://b37f567b3f741c:f43585bf@eu-cdbr-west-03.cleardb.net/heroku_123bc0464991a9f?reconnect=true");
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
