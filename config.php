9863400c7663f908f5565680215cf8975529bd19
efemirusenmez <emir.usenmez@themax.media>
2022-08-12 11:31:17 +0300

    config db duzeltildi

--- config.php
+++ config.php
@@ -1,12 +1,20 @@
 <?php
-$host = "localhost" ;
-$kullanıcıAdı = "fyrhp" ;
-$şifre = "RTDE" ;
-$dbName = "fyrhp" ;
-// Veritabanı bağlantısı oluştur
-$bağlantı = yeni mysqli ( $host, $userName, $password, $dbName ) ;
-// Bağlantıyı kontrol et
-if ( $bağ->connect_error ) {  
-die ( "Bağlantı başarısız: " . $bağ->connect_error ) ;
+include('header.php');
+include('dbconnect.php');
+?>
+<!--?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'eu-cdbr-west-03.cleardb.net');
+define('DB_USERNAME', 'b37f567b3f741c');
+define('DB_PASSWORD', 'f43585bf');
+define('DB_NAME', 'heroku_123bc0464991a9f');
+ 
+/* Attempt to connect to MySQL database */
+$conn = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_123bc0464991a9f', $username, $password);
+ 
+// Check connection
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
