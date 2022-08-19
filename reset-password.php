779fbde901b0e7ee62c5672876120ac1de3ffc31
efemirusenmez <emir.usenmez@themax.media>
2022-08-19 09:26:18 +0300

    reset password güncelleme

--- reset-password.php
+++ reset-password.php
@@ -3,10 +3,6 @@ include('header.php');
 ?>
 
 <?php
-// Initialize the session
-//session_start();
- 
-// Check if the user is logged in, otherwise redirect to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     ("location: login.php");
     exit;
@@ -54,52 +50,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             if($is_user_pass){
                 // Password updated successfully. Destroy the session, and redirect to login page
                 session_destroy();
-                header("location: login.php");
+                echo"Başarıyla değişti.";
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
-            // Close statement
-            //mysqli_stmt_close($stmt);
         }
     }
     
-    // Close connection
-   // mysqli_close($link);
 ?>
  
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Reset Password</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Reset Password</h2>
-        <p>Please fill out this form to reset your password.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
-            <div class="form-group">
-                <label>New Password</label>
-                <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
-                <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-link ml-2" href="welcome.php">Cancel</a>
-            </div>
-        </form>
-    </div>    
-</body>
-</html>
+<div class="wrapper">
+    <h2>Reset Password</h2>
+    <p>Please fill out this form to reset your password.</p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
+        <div class="form-group">
+            <label>New Password</label>
+            <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
+            <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+        </div>
+        <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
+            <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Submit">
+            <a class="btn btn-link ml-2" href="welcome.php">Cancel</a>
+        </div>
+    </form>
+</div>    
