08eed37f76585b97b8070897c9adb6d8138e2931
efemirusenmez <emir.usenmez@themax.media>
2022-08-19 09:34:12 +0300

    reset password güncelleme2

--- reset-password.php
+++ reset-password.php
@@ -57,12 +57,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             }
 
         }
-    }
+}
     
 ?>
  
-<!DOCTYPE html>
-<html lang="en">
 <div class="wrapper">
     <h2>Reset Password</h2>
     <p>Please fill out this form to reset your password.</p>
