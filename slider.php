7295bf3b636780681db062f51c32c3ffdd5efe51
efemirusenmez <emir.usenmez@themax.media>
2022-09-07 10:18:27 +0300

    resim guncelleme

--- slider.php
+++ slider.php
@@ -1,13 +1,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner" style="padding:15px 700px">
+  <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="Fotograflar/Apple.jpg"width="0" height="600" class="d-block w-100" alt="...">
+      <img src="Fotograflar/times-square.webp"width="0" height="600" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="Fotograflar/Apple.jpg" width="0" height="600" class="d-block w-100" alt="...">
+      <img src="Fotograflar/new_york.jpg" width="0" height="600" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="Fotograflar/Apple.jpg"width="250" height="600" class="d-block w-100" alt="...">
+      <img src="Fotograflar/sydney-opera-house.jpg"width="250" height="600" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
